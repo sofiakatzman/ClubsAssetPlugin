6ad58972ad8132bf1ebc31fdf9e6be21ccc845d9
Sofia Katzman <katzmansof@gmail.com>
2024-07-09 11:08:44 -0400

    add logic to generate full size promo assets with correct copy updates

--- code.ts
+++ code.ts
@@ -4,67 +4,87 @@ figma.ui.resize(500, 900);
 figma.loadAllPagesAsync().then(() => {
   figma.ui.onmessage = async pluginMessage => {
     console.log("Received message:", pluginMessage);
-    const nodes:SceneNode[] = [];
-    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
-    let selectedVariant;
+    const nodes: SceneNode[] = [];
+    const fullSizeComponentSet = figma.root.findOne(node => node.type === "COMPONENT_SET" && node.name === "Full Size Test") as ComponentSetNode;
+    let selectedFullVariant;
 
-    if (postComponentSet) {
-      console.log("Found postComponentSet:", postComponentSet);
+    // Load fonts
+    await figma.loadFontAsync({ family: "Filson Pro", style: "Bold" });
+    await figma.loadFontAsync({ family: "Brandon Text", style: "Bold" });
+    await figma.loadFontAsync({ family: "Brandon Text", style: "Regular" });
 
-      // if (pluginMessage.darkModeState === true) {
-      //   switch (pluginMessage.imageVariant) {
-      //     case "2":
-      //       selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=true") as ComponentNode;
-      //       break;
-      //     case "3":
-      //       selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=true") as ComponentNode;
-      //       break;
-      //     default:
-      //       selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
-      //       break;
-      //   }
-      // } else {
-      //   switch (pluginMessage.imageVariant) {
-      //     case "2":
-      //       selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=false") as ComponentNode;
-      //       break;
-      //     case "3":
-      //       selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=false") as ComponentNode;
-      //       break;
-      //     default:
-      //       selectedVariant = postComponentSet.defaultVariant as ComponentNode;
-      //       break;
-      //   }
+    
+    //**************************************** FULL SIZE - ASSET GENERATION LOGIC ****************************************//
+    // check that a full size component was found on figma (template for generated asset)
+    if (fullSizeComponentSet) {
+      console.log("Found postComponentSet:", fullSizeComponentSet);
+
+      // check if full size promo is selected, if it is, then find the selected variant
+      if (pluginMessage.fullSelected === true) {
+        switch (pluginMessage.fullVariant) {
+          case "fs-header-only":
+            selectedFullVariant = fullSizeComponentSet.findOne(node => node.type === "COMPONENT" && node.name === "CTA=No, CTA QTY=0, PreText=No, SubText=No, With Image=False, Discount Code=False") as ComponentNode;
+            break;
+          case "fs-header-subtext":
+            selectedFullVariant = fullSizeComponentSet.findOne(node => node.type === "COMPONENT" && node.name === "CTA=No, CTA QTY=0, PreText=No, SubText=Yes, With Image=False, Discount Code=False") as ComponentNode;
+            break;
+          case "fs-header-cta1":
+            selectedFullVariant = fullSizeComponentSet.findOne(node => node.type === "COMPONENT" && node.name === "CTA=Yes, CTA QTY=1, PreText=No, SubText=No, With Image=False, Discount Code=False") as ComponentNode;
+            break;
+          case "fs-header-cta2":
+            selectedFullVariant = fullSizeComponentSet.findOne(node => node.type === "COMPONENT" && node.name === "CTA=Yes, CTA QTY=2, PreText=No, SubText=No, With Image=False, Discount Code=False") as ComponentNode;
+            break;
+        }
+
+        if (selectedFullVariant) {
+          console.log("Found selectedFullVariant:", selectedFullVariant);
+          // Create an instance based on the selected variant
+          const newFullPromo = selectedFullVariant.createInstance();
+          nodes.push(newFullPromo);
+
+          // Find text fields in the new instance
+          const templateHeader = newFullPromo.findOne(node => node.name === "Header" && node.type === "TEXT") as TextNode;
+          const templateCTA1 = newFullPromo.findOne(node => node.name === "CTA1" && node.type === "TEXT") as TextNode;
+          const templateCTA2 = newFullPromo.findOne(node => node.name === "CTA2" && node.type === "TEXT") as TextNode;
+          const templateSubtext = newFullPromo.findOne(node => node.name === "Subtext" && node.type === "TEXT") as TextNode;
+          const templatePretext = newFullPromo.findOne(node => node.name === "PreText" && node.type === "TEXT") as TextNode;
+          const templateCopyright = newFullPromo.findOne(node => node.name === "Copyright" && node.type === "TEXT") as TextNode;
+
+          // console.log("before assignment testing")
+          // console.log(templateHeader.characters)
+          // console.log(templateCTA1.characters)
+          // console.log(templateCTA2.characters)
+          // console.log(templateSubtext.characters)
+          // console.log(templatePretext.characters)
+          // console.log(templateCopyright.characters)
+
+          // Replace text of new instances
+          if (templateHeader) templateHeader.characters = pluginMessage.header;
+          if (templateCTA1) templateCTA1.characters = pluginMessage.cta1;
+          if (templateCTA2) templateCTA2.characters = pluginMessage.cta2;
+          if (templateSubtext) templateSubtext.characters = pluginMessage.subtext;
+          if (templatePretext) templatePretext.characters = pluginMessage.pretext;
+          if (templateCopyright) templateCopyright.characters = pluginMessage.copyright;
+
+          // console.log("post assignment testing")
+          // if(templateHeader) console.log(templateHeader.characters)
+          // if(templateCTA1) console.log(templateCTA1.characters)
+          // if(templateCTA2) console.log(templateCTA2.characters)
+          // if(templateSubtext) console.log(templateSubtext.characters)
+          // if(templatePretext) console.log(templatePretext.characters)
+          // if(templateCopyright) console.log(templateCopyright.characters)
+          figma.viewport.scrollAndZoomIntoView(nodes);
+        } else {
+          console.error("No matching component found for the given criteria.");
+        }
       }
+    } else {
+      console.error("Component set with the name 'Full Size' not found");
+    }
 
-    //   if (selectedVariant) {
-    //     console.log("Found selectedVariant:", selectedVariant);
-    //     // create instance based on selected variant
-    //     const newPost = selectedVariant.createInstance();
-    //     // find text fields in new instance
-    //     const templateName= newPost.findOne(node => node.name == "displayName" && node.type == "TEXT") as TextNode;
-    //     const templateUserName= newPost.findOne(node => node.name == "@username" && node.type == "TEXT") as TextNode;
-    //     const templateDescription= newPost.findOne(node => node.name == "description" && node.type == "TEXT") as TextNode;
-        
-    //     // Load fonts
-    //     await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
-    //     nodes.push(newPost)
-    //     // Replace text of new instances
-    //     templateName.characters = pluginMessage.name;
-    //     templateUserName.characters = pluginMessage.username;
-    //     templateDescription.characters = pluginMessage.description;
-    //   } else {
-    //     console.error("No matching component found for the given criteria.");
-    //   }
-    // } else {
-    //   console.error("Component set with the name 'post' not found");
-    // }
-    
-    
-    figma.viewport.scrollAndZoomIntoView(nodes);
-    figma.closePlugin();
+    // figma.closePlugin();
   };
 }).catch(error => {
   console.error("Error loading pages:", error);
-  figma.closePlugin();
+  // figma.closePlugin();
 });
