084945e21b77ee058cac6cb3314264742c82f012
Sofia Katzman <katzmansof@gmail.com>
2024-08-23 11:24:11 -0400

    Initial commit for new branch

---
+++ src/app/components/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../styles/ui.css';
+
+function App() {
+  const textbox = React.useRef<HTMLInputElement>(null);
+
+  const onSync = () => {
+    const airtableURL = textbox.current?.value;
+    parent.postMessage({ pluginMessage: { type: 'bulk-processing', airtableURL } }, '*');
+  };
+
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = (event) => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === 'bulk-processing') {
+        console.log(`Figma Says : ${message}`);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Bulk Asset Creation</h2>
+      <p>
+        Airtable URL: <input ref={textbox} />
+      </p>
+      <button id="create" onClick={onSync}>
+        Sync
+      </button>
+    </div>
+  );
+}
+
+export default App;
